b7d6df96fffbb6d7f7d37d885ef30d6b070ebc59
mpsddd-commits <mpsddd@gmail.com>
2026-04-14 15:46:24 +0900

    가영이가 노래 할거야?

---
+++ mariadb/ddl.sql
@@ -0,0 +1,46 @@
+-- --------------------------------------------------------
+-- 호스트:                          127.0.0.1
+-- 서버 버전:                        12.3.1-MariaDB - MariaDB Server
+-- 서버 OS:                        Linux
+-- HeidiSQL 버전:                  12.13.0.7147
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- edu 데이터베이스 구조 내보내기
+USE `edu`;
+
+-- 테이블 edu.ai_agent 구조 내보내기
+CREATE TABLE IF NOT EXISTS `ai_agent` (
+  `no` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`no`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 테이블 데이터 edu.ai_agent:~9 rows (대략적) 내보내기
+INSERT INTO `ai_agent` (`no`, `name`, `title`, `content`) VALUES
+	(8, '김지환', '좋은날', '하하 기분이 좋구나~'),
+	(10, '하하', '그래?', '기분이 좋아요~'),
+	(11, '하하', '그래?', '기분이 좋아요~'),
+	(12, '하하', '그래?', '기분이 좋아요~'),
+	(13, '하하', '아하아하', '에이 그런가?'),
+	(14, '하하', '그래?', '기분이 좋아요~'),
+	(19, '김지환', '김지환', '김지환'),
+	(20, '김지환', '김지환', '김지환'),
+	(21, '하이', '김', '지환의 글 작성');
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
